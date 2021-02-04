96e3c98c6fcaa9d713696174f155a36cada2c972
lime <osmium760@gmail.com>
2021-02-04 18:34:09 +0300

    add mailbox

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",     "cat /tmp/recordingicon 2>/dev/null",   0,     9},
+
 	{"",     "player",                               0,     11},
 
-	{"",     "cat /tmp/recordingicon 2>/dev/null",   0,     9},
+	{"",     "mailbox",                             10,     13},
 
 	//{"",     "weather",                             600,     0},
 
